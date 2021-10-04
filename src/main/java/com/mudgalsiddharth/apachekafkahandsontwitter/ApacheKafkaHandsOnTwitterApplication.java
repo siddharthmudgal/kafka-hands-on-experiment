35497acc13bc7d276dd959aa9f9c9fe7f0063113
Siddharth Mudgal <siddharthmudgal@Siddharths-MacBook-Pro-2.local>
2021-10-04 20:29:23 +0800

    build.yml added with workflow

---
+++ src/main/java/com/mudgalsiddharth/apachekafkahandsontwitter/ApacheKafkaHandsOnTwitterApplication.java
@@ -0,0 +1,13 @@
+package com.mudgalsiddharth.apachekafkahandsontwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheKafkaHandsOnTwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApacheKafkaHandsOnTwitterApplication.class, args);
+	}
+
+}
